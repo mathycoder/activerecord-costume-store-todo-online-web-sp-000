8c6ab529b8d56b6866acbd106995eb5b86347b76
Adam S <adam.sarli@gmail.com>
2019-04-14 16:17:39 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,9 +7,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name 
       t.string :location 
       t.integer :costume_inventory 
-      t.integer :employees_count
-      
-    
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.string :opening_time
+      t.string :end_time 
   end 
   
 end 
