71676fe4503864c0d333bea287898d7dc41d9f16
Adam S <adam.sarli@gmail.com>
2019-04-14 16:22:53 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme 
       t.float :price 
-      
+      t.boolean :family_friendly
+      t.string :opening_date 
+      t.string :closing_date 
     end 
   end 
   
