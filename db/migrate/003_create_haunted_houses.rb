5668456dccaa90f3bcfd78ed38d97d571762303f
Adam S <adam.sarli@gmail.com>
2019-04-14 16:21:57 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,6 +4,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   
   def change 
     create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme 
+      
     end 
   end 
   
